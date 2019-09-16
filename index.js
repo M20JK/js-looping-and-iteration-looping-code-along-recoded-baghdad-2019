98cf948ea0ad79d0aa4382c784293be1fc9c3833
Mohammed Jawad <al7joker111@gmail.com>
2019-09-16 12:52:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(name , eventName){
+  var thank = [];
+  for(let r = 0 ; r < name.length; r++ ){
+   
+   thank[r] = (`Thank you, ${name[r]}, for the wonderful ${eventName} gift!`);
+  }
+  
+  return thank ;
+  
+  
+}
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+
+function countdown(numb){
+    for (let d = numb ; d >= 0 ; d --){
+    console.log(d);
+    }
+
+}
+countdown(10)
