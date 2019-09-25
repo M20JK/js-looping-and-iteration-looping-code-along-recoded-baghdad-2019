fab3cf00ebfb6be5ef5d2aa902dc74418cbc961c
Mohammed Jawad <al7joker111@gmail.com>
2019-09-25 14:18:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ function writeCards(name , eventName){
   var thank = [];
   for(let r = 0 ; r < name.length; r++ ){
    
+<<<<<<< HEAD
    thank[r] = (`Thank you, ${name[r]}, for the wonderful ${eventName} gift!`);
+=======
+   thank[r] = (`Thank you, ${name[r]}, for your special gift in my ${eventName}!`);
+>>>>>>> 14aa4a7ea6f018756060043d24e581c3c5c14373
   }
   
   return thank ;
